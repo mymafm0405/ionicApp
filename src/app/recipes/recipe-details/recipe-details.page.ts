5565095b51f08df3e6fb2ed8a19b3e98433ba55d
Mahmoud <mymafm0405@gmail.com>
2021-11-30 19:54:58 +0300

    Now details is working

--- src/app/recipes/recipe-details/recipe-details.page.ts
+++ src/app/recipes/recipe-details/recipe-details.page.ts
@@ -1,4 +1,7 @@
+import { Recipe } from './../recipe.model';
+import { RecipesService } from './../recipes.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-details',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-details.page.scss'],
 })
 export class RecipeDetailsPage implements OnInit {
+  loadedRecipe: Recipe;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private recipesService: RecipesService
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe((recId: Params) => {
+      const id = recId.recipeId;
+      this.loadedRecipe = this.recipesService.getRecipe(id);
+    });
   }
-
 }
